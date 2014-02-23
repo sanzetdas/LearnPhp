8bfc2bf4cdd50fb36b9bea5729ec7715ed195ab4
Sanjeet Das <sanzetdas@gmail.com>
2014-02-23 13:57:45 +0545

    varibles of php

---
+++ variables/index.php
@@ -0,0 +1,8 @@
+<?php
+	// Variables are "containers" for storing information
+	$x=5; // assigning integer 5 to variable $x 
+	$y=6;
+ 
+	$z=$x+$y; // adding two varibles $x and $y and assigning the sum to varible $z
+	echo $z; // print $z
+?>
