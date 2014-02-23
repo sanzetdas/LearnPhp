40fd45b6d2b51533aeec2c2beebc2d58346747e3
Sanjeet Das <sanzetdas@gmail.com>
2014-02-23 14:06:14 +0545

    print and echo tutorials

---
+++ print-echo/index.php
@@ -0,0 +1,12 @@
+<?php
+	
+	// In PHP there is two basic ways to get output: echo and print.
+	//echo - can output one or more strings
+	//print - can only output one string, and returns always 1
+	
+	echo "Hello world!<br>"; // Echo normal php strings Hello World
+	echo "This", " string", " was", " made", " with multiple parameters."; //  Echo muliple Stings
+
+	print("Hello World"); // Print function used to print hello world
+
+?>
--- variables/index.php
+++ variables/index.php
@@ -1,8 +1,10 @@
 <?php
+	
 	// Variables are "containers" for storing information
 	$x=5; // assigning integer 5 to variable $x 
 	$y=6;
  
 	$z=$x+$y; // adding two varibles $x and $y and assigning the sum to varible $z
 	echo $z; // print $z
+	
 ?>
